798e5dfb20a2a983d9040ff6a9701246a5db8da6
noorDaoud <noor.daoud@mail.huji.ac.il>
2025-04-02 17:19:23 +0300

    adding movments to the player and prfabs.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]private float moveSpeed = 5f; // Speed of the player
+    private Rigidbody2D _rb;
+    private Vector2 _movement;
+
+    // Start is called before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>(); // Getting the Rigidbody2D component attached to the player
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Getting input from keyboard (WASD or Arrow keys)
+        _movement.x = Input.GetAxisRaw("Horizontal"); // A, D, Left Arrow, Right Arrow
+        _movement.y = Input.GetAxisRaw("Vertical"); // W, S, Up Arrow, Down Arrow
+    }
+
+    // FixedUpdate is called at a fixed interval and is used for handling physics
+    void FixedUpdate()
+    {
+        // Moving the player using Rigidbody2D
+        _rb.MovePosition(_rb.position + _movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
