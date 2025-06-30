0d586d0141076e427c46588cb7adefbf0b884474
noorDaoud <noor.daoud@mail.huji.ac.il>
2025-06-30 21:12:22 +0300

    fixing the player shooting.

--- Assets/_LTD/Scripts/GameLogic/Controls/LTDPlayer.cs
+++ Assets/_LTD/Scripts/GameLogic/Controls/LTDPlayer.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 namespace LTD.GameLogic.Controls
 {
-
     public class LTDPlayer : LTDBaseMono
     {
-        [Header("Settings")] [SerializeField] private float moveSpeed = 5f;
+        [Header("Settings")] 
+        [SerializeField] private float moveSpeed = 5f;
+
+        [Header("References")]
+        [SerializeField] private LTDWand wand; 
+
         private Vector3 _direction;
-        
-        public void Update()
+
+        private void Update()
         {
             CheckInput();
         }
 
-        
         private void CheckInput()
         {
             var horizontal = Input.GetAxis("Horizontal");
@@ -24,7 +27,11 @@ namespace LTD.GameLogic.Controls
             _direction = new Vector3(horizontal, vertical, 0).normalized;
 
             transform.position += _direction * (Time.deltaTime * moveSpeed);
-        }
 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                wand?.Fire();
+            }
+        }
     }
 }
--- Assets/_LTD/Scripts/GameLogic/Controls/LTDWand.cs
+++ Assets/_LTD/Scripts/GameLogic/Controls/LTDWand.cs
@@ -9,24 +9,11 @@ namespace _LTD.Scripts.GameLogic
 {
     public class LTDWand:LTDBaseMono
     {
-        [SerializeField] private float fireRate;
         [SerializeField] private LTDBaseProjectile spell;
-        [FormerlySerializedAs("enemiesProvider")] [SerializeField] private LTDSmallDevilProvider smallDevilProvider;
+        [FormerlySerializedAs("enemiesProvider")] 
+        [SerializeField] private LTDSmallDevilProvider smallDevilProvider;
         
-        private float _timer;
-        private void Update()
-        {
-            _timer += Time.deltaTime;
-
-            if (_timer >= fireRate )
-            {
-                Fire();
-                _timer = 0;
-            }   
-        }
-
-        
-        private void Fire()
+        public void Fire()
         {
             var projectile = Instantiate(spell, transform.position, Quaternion.identity);
             
